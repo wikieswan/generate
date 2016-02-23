e15e9fb33c9d34656a226058599f6ee689d8606b
wikies.wan <1175551641@qq.com>
2016-02-23 18:16:22 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat'),
-    sh = require('shelljs'),
     plumber = require( 'gulp-plumber'),
-    gulpif = require('gulp-if'),
-    argv = require('yargs').argv,
     size = require('gulp-size'),
-
-    //serve
     connect = require('gulp-connect'),
-
-    //css
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     minifyCss = require('gulp-minify-css'),
-
-    //js
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     uglify = require('gulp-uglify'),
-
     packageJson = require('./package.json'),
-
     del = require('del');
 var exec = require('child_process').exec;
 var version  = packageJson.version;
@@ -30,90 +18,60 @@ var version  = packageJson.version;
 var path = {};
 path.root="./";
 
-path.css  = path.root + 'css/**/*.css';
+path.css   = path.root + 'css/**/*.css';
+path.cssDir= path.root + 'css/';
 path.dist  = path.root + 'dist/';
 path.html  = path.root + 'html/**/*.html';
 path.image = path.root + 'image/**/*.*';
 path.js    = path.root + 'js/**/*.js';
+path.jsDir = path.root + 'js/';
 path.scss  = path.root + 'scss/**/*.scss';
 
 
-path.copyFile = [
-    path.root + 'assets/fonts/*.*'
-];
-
-
-var cors = function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-};
 gulp.task('connect', function() {
     connect.server({
-        root: '../vpalmobileui/',
+        root: './',
         port: 3000,
-        livereload: true,
-        middleware: function () {
-          return [cors];
-        }
-    });
+        livereload: true
+    })
 });
 
 // clean dist directory
 gulp.task('clean', function (callback) {
-    return del([path.build + '*'], callback);
+    return del([path.dist + 'css/*'], callback);
 });
-
-// copy base files from src to dist
-gulp.task('copy', function () {
-    return gulp.src(path.copyFile, {base: path.root })
-        .pipe(gulp.dest(path.build));
-});
-
 
 //sass build
-gulp.task('style', function() {
-    console.log(path.scss)
+gulp.task('scss', function() {
     gulp.src(path.scss)
         .pipe( plumber( {errorHandler : errrHandler} ) )
         .pipe(sass({
             errLogToConsole: true
         }))
         .pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest(path.scssBuild))
+        .pipe(gulp.dest(path.cssDir))
         .pipe(minifyCss())
         .pipe(rename({ extname: '-'+version+'.min.css' }))
-        .pipe(gulp.dest(path.scssBuild));
+        .pipe(gulp.dest(path.cssDir));
         
 });
-
-
 
 function errrHandler( e ){
     gutil.beep();
     gutil.log('===================',gutil.colors.cyan(e));
 }
 
-gulp.task('script', function() {
+gulp.task('js', function() {
      gulp.src(path.js,{base: path.root })
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(size())
-        .pipe(gulp.dest(path.build))
+        .pipe(gulp.dest(path.dist))
         .pipe(uglify({output: {ascii_only:true}}))
         .pipe(rename({ extname: '-'+version+'.min.js' }))
-        .pipe(gulp.dest(path.build));
+        .pipe(gulp.dest(path.dist));
 });
-
-gulp.task('concat', function() {
-    gulp.src([
-            path.assets + 'javascripts/Vpal.ui.js',
-            path.assets + 'javascripts/Vpal.util.js'
-        ])
-        .pipe(concat('vpal.all.js'))
-        .pipe(gulp.dest(path.assets + 'javascripts'));
-    gulp.start(['script']);
-});    
+  
 
 gulp.task('reload',function() {
     gulp.src([path.html,path.scss,path.js])
@@ -121,8 +79,8 @@ gulp.task('reload',function() {
 });
 
 gulp.task('watch', function () {
-    gulp.watch([path.scss], ['style']);
-    gulp.watch([path.js], ['script']);
+    gulp.watch([path.scss], ['scss']);
+    gulp.watch([path.js], ['js']);
     gulp.watch([path.html,path.scss,path.js],['reload']);
 });
 
@@ -131,9 +89,12 @@ gulp.task('serve', function() {
 });
 
 gulp.task('build',function(){
-    gulp.start(['concat', 'style']);
+    gulp.start('clean',function(){
+        gulp.src([path.css],{base: path.root })
+            .pipe(gulp.dest(path.dist));
+    })
+    
 });
-
 
 gulp.task('default', ['serve']);
 
